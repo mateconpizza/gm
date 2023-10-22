04486f78471e0be2f1094b1ddb13de3ec6e2bf3a
haaag <81921095+haaag@users.noreply.github.com>
2023-10-22 12:10:42 -0300

    refactor: `main` clean up

--- cmd/main.go
+++ cmd/main.go
@@ -1,75 +1,54 @@
 package main
 
+// TODO:
+// = [ ] add sub-commands
+// = [X] add format option to json, pretty, color-pretty
+
 import (
 	"flag"
 	"fmt"
+	"gomarks/pkg/cli"
 	"gomarks/pkg/constants"
 	"gomarks/pkg/database"
 	"gomarks/pkg/display"
 	m "gomarks/pkg/menu"
-	"gomarks/pkg/utils"
+	u "gomarks/pkg/utils"
 	"log"
+	"os"
 )
 
 var (
-	menuName    string
-	byQuery     string
-	jsonFlag    bool
-	testFlag    bool
-	optionsFlag bool
-	deleteFlag  bool
 	addFlag     bool
-	dropDB      bool
-	migrateData bool
+	byQuery     string
+	byTag       string
+	deleteFlag  bool
+	format      string
+	limit       int
+	menuName    string
+	testFlag    bool
 	verboseFlag bool
 )
 
 func init() {
-	flag.StringVar(&menuName, "m", "rofi", "name of the menu [dmenu rofi]")
-	flag.StringVar(&byQuery, "q", "", "query to filter bookmarks")
-	flag.BoolVar(&jsonFlag, "json", false, "JSON output")
-	flag.BoolVar(&verboseFlag, "v", false, "Enable verbose output")
 	flag.BoolVar(&addFlag, "add", false, "add a bookmark")
-	flag.BoolVar(&testFlag, "test", false, "test mode")
-	flag.BoolVar(&optionsFlag, "options", false, "show options")
 	flag.BoolVar(&deleteFlag, "delete", false, "delete a bookmark")
-	flag.BoolVar(&dropDB, "drop", false, "drop the database")
-	flag.BoolVar(&migrateData, "migrate", false, "migrate data")
+	flag.BoolVar(&testFlag, "test", false, "test mode")
+	flag.BoolVar(&verboseFlag, "v", false, "enable verbose output")
+	flag.IntVar(&limit, "limit", 0, "limit number of bookmarks")
+	flag.StringVar(&byQuery, "query", "", "query to filter bookmarks")
+	flag.StringVar(&byTag, "tag", "", "filter bookmarks by tag")
+	flag.StringVar(&format, "f", "", "output format [json|pretty|color-pretty]")
+	flag.StringVar(&menuName, "m", "rofi", "name of the menu [dmenu rofi]")
 }
 
-func main() {
-	var tableName string = constants.DBMainTableName
-	flag.Parse()
-
-	// Set log level
-	utils.SetLogLevel(verboseFlag)
-
-	// Set up the home project
-	utils.SetupHomeProject()
-
-	// Load menus
-	mc := make(m.MenuCollection)
-	mc.Load()
-	menu, err := mc.Get(menuName)
+func parseAndExit(r *database.SQLiteRepository, flags *flag.FlagSet, menu *m.Menu) {
+	err := flags.Parse(os.Args[1:])
 	if err != nil {
-		log.Fatal("Error getting menu:", err)
-	}
-
-	r := database.GetDB()
-	defer r.DB.Close()
-
-	if *dropDB {
-		r.HandleDropDB()
-		return
-	}
-
-	if testFlag {
-		display.HandleTestMode(&menu, r)
-		return
+		log.Fatal(err)
 	}
 
 	if addFlag {
-		b, err := display.AddBookmark(r, &menu)
+		b, err := display.AddBookmark(r, menu)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -77,24 +56,55 @@ func main() {
 		fmt.Println(j)
 		return
 	}
+}
 
-	if optionsFlag {
-		// display.HandleOptionsMode(&menu)
-		return
+func getMenu(s string) m.Menu {
+	mc := make(m.MenuCollection)
+	mc.Load()
+	menu, err := mc.Get(menuName)
+	if err != nil {
+		log.Fatal("Error getting menu:", err)
 	}
+	return menu
+}
 
-	if migrateData {
-		tableName = constants.DBDeletedTableName
-	}
+func main() {
+	tableName := constants.DBMainTableName
+	var bookmarks []database.Bookmark
+	var err error
 
-	bookmarks, err := database.FetchBookmarks(r, byQuery, tableName)
+	fmt.Printf("Args: %s\n\n", os.Args)
+	flag.Parse()
+
+	// Set log level
+	u.SetLogLevel(verboseFlag)
+
+	// Set up the home project
+	u.SetupHomeProject()
+
+	// Load menus
+	menu := getMenu(menuName)
+
+	r := database.GetDB()
+	defer r.DB.Close()
+
+	parseAndExit(r, flag.CommandLine, &menu)
+
+	bookmarks, err = database.FetchBookmarks(r, byQuery, byTag, tableName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if jsonFlag {
-		j := database.ToJSON(&bookmarks)
-		fmt.Println(j)
+	if limit > 0 {
+		if len(bookmarks) > limit {
+			bookmarks = bookmarks[:limit]
+		}
+	}
+
+	if format != "" {
+		if err := cli.HandleFormat(format, bookmarks); err != nil {
+			log.Fatal(err)
+		}
 		return
 	}
 
