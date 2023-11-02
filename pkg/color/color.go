ab957f45d4b1675bfa9d494c507187c5bb646bc1
haaag <81921095+haaag@users.noreply.github.com>
2023-11-02 10:56:39 -0300

    chore: housekeeping..

--- pkg/color/color.go
+++ pkg/color/color.go
@@ -5,16 +5,18 @@ import (
 	"time"
 )
 
-var Blue = "\033[34m"
-var Cyan = "\033[36m"
-var Gray = "\033[37;2m"
-var Green = "\033[32m"
-var Purple = "\033[35m"
-var Red = "\033[31m"
-var White = "\033[97m"
-var Yellow = "\033[33m"
-var Bold = "\033[1m"
-var Reset = "\033[0m"
+var (
+	Blue   = "\033[34m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37;2m"
+	Green  = "\033[32m"
+	Purple = "\033[35m"
+	Red    = "\033[31m"
+	White  = "\033[97m"
+	Yellow = "\033[33m"
+	Bold   = "\033[1m"
+	Reset  = "\033[0m"
+)
 
 func GetRandomColor() string {
 	colors := []string{Red, Green, Yellow, Blue, Purple, Cyan, Gray, White}
--- pkg/display/display.go
+++ pkg/display/display.go
@@ -1,44 +1,18 @@
 package display
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"strings"
-	"time"
 
 	bm "gomarks/pkg/bookmark"
 	c "gomarks/pkg/constants"
 	db "gomarks/pkg/database"
 	"gomarks/pkg/menu"
-	"gomarks/pkg/scrape"
 	u "gomarks/pkg/util"
 )
 
-/* func ShowOptions(m *menu.Menu) (int, error) {
-	options := []fmt.Stringer{
-		menu.Option{"Add a bookmark"},
-		menu.Option{"Edit a bookmark"},
-		menu.Option{"Delete a bookmark"},
-		menu.Option{"Exit"},
-	}
-	idx, err := m.Select(options)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return idx, nil
-}
-
-func PavelOptions(menuArgs []string) (int, error) {
-	optionsMap := make(map[string]interface{})
-	optionsMap["Add a bookmark"] = addBookmark
-	optionsMap["Edit a bookmark"] = editBookmark
-	optionsMap["Delete a bookmark"] = DeleteBookmark
-	optionsMap["Exit"] = nil
-	return -1, nil
-} */
-
-func AddBookmark(r *db.SQLiteRepository, m *menu.Menu) (bm.Bookmark, error) {
+/* func AddBookmark(r *db.SQLiteRepository, m *menu.Menu) (bm.Bookmark, error) {
 	currentTime := time.Now()
 	currentTimeString := currentTime.Format("2006-01-02 15:04:05")
 
@@ -71,9 +45,9 @@ func AddBookmark(r *db.SQLiteRepository, m *menu.Menu) (bm.Bookmark, error) {
 		return bm.Bookmark{}, err
 	}
 	return b, nil
-}
+} */
 
-func EditBookmark(r *db.SQLiteRepository, m *menu.Menu, b *bm.Bookmark) (bm.Bookmark, error) {
+/* func EditBookmark(r *db.SQLiteRepository, m *menu.Menu, b *bm.Bookmark) (bm.Bookmark, error) {
 	m.UpdatePrompt(fmt.Sprintf("Editing ID: %d", b.ID))
 	s, err := m.Run(b.String())
 	if err != nil {
@@ -81,7 +55,7 @@ func EditBookmark(r *db.SQLiteRepository, m *menu.Menu, b *bm.Bookmark) (bm.Book
 	}
 	fmt.Println(s)
 	return *b, nil
-}
+} */
 
 func DeleteBookmark(r *db.SQLiteRepository, m *menu.Menu, b *bm.Bookmark) error {
 	msg := fmt.Sprintf("Deleting bookmark: %s", b.URL)
@@ -105,14 +79,6 @@ func DeleteBookmark(r *db.SQLiteRepository, m *menu.Menu, b *bm.Bookmark) error 
 	return nil
 }
 
-/* func HandleOptionsMode(m *menu.Menu) {
-	idx, err := ShowOptions(m)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Selected:", idx)
-} */
-
 func HandleTestMode(m *menu.Menu, r *db.SQLiteRepository) {
 	fmt.Print("\n::::::::Start Test Mode::::::::\n\n")
 	fmt.Print("\n::::::::End Test Mode::::::::\n\n")
@@ -120,7 +86,8 @@ func HandleTestMode(m *menu.Menu, r *db.SQLiteRepository) {
 
 func SelectBookmark(m *menu.Menu, bookmarks *bm.BookmarkSlice) (bm.Bookmark, error) {
 	var itemsText []string
-	m.UpdateMessage(fmt.Sprintf(" Welcome to GoMarks\n Showing (%d) bookmarks", len(*bookmarks)))
+	msg := fmt.Sprintf(" Welcome to GoMarks\n Showing (%d) bookmarks", len(*bookmarks))
+	m.UpdateMessage(msg)
 	log.Printf("Selecting bookmark from %d bookmarks\n", len(*bookmarks))
 
 	for _, bm := range *bookmarks {
@@ -147,18 +114,4 @@ func SelectBookmark(m *menu.Menu, bookmarks *bm.BookmarkSlice) (bm.Bookmark, err
 		return b, nil
 	}
 	return bm.Bookmark{}, fmt.Errorf("item not found: %s", selectedStr)
-}
-
-func HandleAction(bmarks bm.BookmarkSlice, c, o bool) error {
-	if len(bmarks) == 0 {
-		return fmt.Errorf("no bookmarks found")
-	}
-	if c {
-		bmarks[0].CopyToClipboard()
-	}
-	if o {
-		fmt.Println("Not implemented yet")
-		fmt.Println(bmarks[0])
-	}
-	return nil
 }
