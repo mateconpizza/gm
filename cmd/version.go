8b9273ee5f4dc3c313b6055565dfd8d07033276e
haaag <81921095+haaag@users.noreply.github.com>
2023-11-08 18:09:29 -0300

    feat: add `version` command

---
+++ cmd/version.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"gomarks/pkg/constants"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "show version",
+	Args:  cobra.NoArgs,
+	Run: func(_ *cobra.Command, _ []string) {
+		fmt.Printf("%s v%s\n", constants.AppName, constants.Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
