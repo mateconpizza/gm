8589bc5b7a6637c29fe78f89966085a7199cfde5
haaag <81921095+haaag@users.noreply.github.com>
2023-11-14 18:00:21 -0300

    test(scrape): better tests

--- pkg/scrape/scrape_test.go
+++ pkg/scrape/scrape_test.go
@@ -1,27 +1,118 @@
 package scrape
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestTitleAndDescription(t *testing.T) {
-	url := "https://old.reddit.com"
-
-	title, err := GetTitle(url)
-	if err != nil {
-		t.Fatalf("Error scraping: %v", err)
+func TestGetTitle(t *testing.T) {
+	tests := []struct {
+		name     string
+		url      string
+		server   *httptest.Server
+		expected string
+		wantErr  bool
+	}{
+		{
+			name:     "ValidTitle",
+			url:      "http://example.com",
+			server:   createTestServer(`<title>Test Title</title>`),
+			expected: "Test Title",
+			wantErr:  false,
+		},
+		{
+			name:     "NoTitleTag",
+			url:      "http://example.com",
+			server:   createTestServer(`<h1>Test Heading</h1>`),
+			expected: TitleDefault,
+			wantErr:  false,
+		},
+		{
+			name:     "HTTPError",
+			url:      "http://invalid-url",
+			server:   nil,
+			expected: TitleDefault,
+			wantErr:  true,
+		},
 	}
 
-	if title == "" {
-		t.Error("Title is empty")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.server != nil {
+				defer tt.server.Close()
+				tt.url = tt.server.URL
+			}
+
+			got, err := GetTitle(tt.url)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetTitle() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got != tt.expected {
+				t.Errorf("GetTitle() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+}
+
+func TestGetDescription(t *testing.T) {
+	tests := []struct {
+		name     string
+		url      string
+		server   *httptest.Server
+		expected string
+		wantErr  bool
+	}{
+		{
+			name:     "ValidDescription",
+			url:      "http://example.com",
+			server:   createTestServer("<html><head><meta name=\"description\" content=\"Test Description\"></head></html>"),
+			expected: "Test Description",
+			wantErr:  false,
+		},
+		{
+			name:     "NoDescriptionMetaTag",
+			url:      "http://example.com",
+			server:   createTestServer(`<h1>Test Heading</h1>`),
+			expected: DescDefault,
+			wantErr:  false,
+		},
+		{
+			name:     "HTTPError",
+			url:      "http://invalid-url",
+			server:   nil,
+			expected: DescDefault,
+			wantErr:  true,
+		},
 	}
 
-	description, err := GetDescription(url)
-	if err != nil {
-		t.Fatalf("Error scraping: %v", err)
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.server != nil {
+				defer tt.server.Close()
+				tt.url = tt.server.URL
+			}
 
-	if description == "" {
-		t.Error("Description is empty")
+			got, err := GetDescription(tt.url)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetDescription() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got != tt.expected {
+				t.Errorf("GetDescription() = %v, want %v", got, tt.expected)
+			}
+		})
 	}
+}
+
+func createTestServer(responseBody string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, responseBody)
+	}))
 }
