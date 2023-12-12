4e41fe9bb4a686dbce9cfa6836d783ef70f37aa7
haaag <81921095+haaag@users.noreply.github.com>
2023-12-12 16:23:04 -0300

    refactor(cmd): remove records using editor `buffer`

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,8 +1,8 @@
-/*
-Copyright © 2023 haaag <git.haaag@gmail.com>
-*/package cmd
+// Copyright © 2023 haaag <git.haaag@gmail.com>
+package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"gomarks/pkg/app"
@@ -16,12 +16,13 @@ import (
 )
 
 var deleteCmd = &cobra.Command{
-	Use:          "delete",
-	Short:        "delete a bookmark by query",
-	Long:         "delete a bookmark by query or id",
-	Example:      exampleUsage([]string{"delete\n", "delete <id>\n", "delete <query>"}),
-	SilenceUsage: true,
+	Use:     "del",
+	Short:   "delete a bookmark by query",
+	Long:    "delete a bookmark by query or id",
+	Example: exampleUsage("delete <id>\n", "delete <query>\n", "delete <id id id>"),
 	RunE: func(_ *cobra.Command, args []string) error {
+		var proceed bool
+
 		r, err := getDB()
 		if err != nil {
 			return fmt.Errorf("%w", err)
@@ -32,41 +33,40 @@ var deleteCmd = &cobra.Command{
 			return fmt.Errorf("%w", err)
 		}
 
-		format.CmdTitle("delete mode")
+		for {
+			util.CleanTerm()
+			s := format.Text(fmt.Sprintf("Bookmarks to delete [%d]:", len(*bs))).Red().Bold()
+			printSliceSummary(bs, s.String())
 
-		bFound := fmt.Sprintf("[%d] bookmarks found\n", bs.Len())
-		bf := color.Colorize(bFound, color.Red)
-		fmt.Println(bf)
+			proceed, err = confirmProceed(bs, bookmark.EditionSlice)
 
-		toDel, err := parseSliceDel(bs)
-		if err != nil {
-			return fmt.Errorf("parsing slice: %w", err)
+			if !errors.Is(err, bookmark.ErrBufferUnchanged) && err != nil {
+				return err
+			}
+
+			if proceed {
+				break
+			}
 		}
 
-		if err = deleteAndReorder(r, &toDel); err != nil {
+		if len(*bs) == 0 {
+			return fmt.Errorf("%w", errs.ErrActionAborted)
+		}
+
+		if err = deleteAndReorder(r, bs); err != nil {
 			return fmt.Errorf("deleting and reordering records: %w", err)
 		}
 
-		total := fmt.Sprintf("[%d] bookmarks deleted.\n", toDel.Len())
-		deleting := color.Colorize(total, color.Red)
-		fmt.Printf("%s%s\n", color.Bold, deleting)
+		total := fmt.Sprintf("\n[%d] bookmarks deleted\n", len(*bs))
+		deleting := format.Text(total).Red()
+		fmt.Println(deleting)
 
 		return nil
 	},
 }
 
-func init() {
-	rootCmd.AddCommand(deleteCmd)
-}
-
-/**
- * Deletes the specified bookmarks from the database and reorders the remaining IDs.
- *
- * @param r The SQLite repository to use for accessing the database.
- * @param toDel A pointer to a `bookmark.Slice` containing the bookmarks to be deleted.
- *
- * @return An error if any occurred during deletion or reordering.
- */
+// deleteAndReorder deletes the specified bookmarks from the database and
+// reorders the remaining IDs.
 func deleteAndReorder(r *database.SQLiteRepository, toDel *bookmark.Slice) error {
 	if err := r.DeleteRecordsBulk(app.DB.Table.Main, toDel.IDs()); err != nil {
 		return fmt.Errorf("deleting records in bulk: %w", err)
@@ -76,53 +76,13 @@ func deleteAndReorder(r *database.SQLiteRepository, toDel *bookmark.Slice) error
 		return fmt.Errorf("reordering ids: %w", err)
 	}
 
+	if err := r.InsertRecordsBulk(app.DB.Table.Deleted, toDel); err != nil {
+		return fmt.Errorf("inserting records in bulk after deletion: %w", err)
+	}
+
 	return nil
 }
 
-/**
- * Interactively prompts the user to select bookmarks for deletion and constructs a slice of the selected bookmarks.
- *
- * @param bs A slice of bookmarks from which to select.
- *
- * @return A slice of the selected bookmarks for deletion, or an error if no bookmarks were selected.
- */
-func parseSliceDel(bs *bookmark.Slice) (bookmark.Slice, error) {
-	if bs.Len() == 0 {
-		return nil, fmt.Errorf("%w", errs.ErrBookmarkNotSelected)
-	}
-
-	var toDel bookmark.Slice
-
-	for i, b := range *bs {
-		fmt.Println(b.String())
-
-		// Prompt the user to confirm deletion for each bookmark.
-		if confirm := util.Confirm(fmt.Sprintf("Delete bookmark [%d/%d]?", i+1, bs.Len())); confirm {
-			toDel = append(toDel, b)
-			if bs.Len() > 1 {
-				fmt.Println(color.Colorize("Added to delete queue", color.Red))
-			}
-		}
-		fmt.Println()
-	}
-
-	// Check if any bookmarks were selected for deletion.
-	if toDel.Len() == 0 {
-		return nil, fmt.Errorf("slice to delete: %w", errs.ErrBookmarkNotSelected)
-	}
-
-	// If multiple bookmarks were selected, summarize the deletion and
-	// prompt for final confirmation.
-	if toDel.Len() > 1 && !confirmDeletion(&toDel) {
-		return nil, fmt.Errorf("%w", errs.ErrActionAborted)
-	}
-
-	return toDel, nil
-}
-
-func confirmDeletion(toDel *bookmark.Slice) bool {
-	d := fmt.Sprintf("Bookmarks to delete [%d]", toDel.Len())
-	fmt.Println(color.ColorizeBold(d, color.Red))
-	printSliceSummary(toDel)
-	return util.Confirm("Do you want to continue?")
+func init() {
+	rootCmd.AddCommand(deleteCmd)
 }
