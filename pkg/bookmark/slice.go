2ca3283b2528c6e5e86aeb7b5ec25d533ad10db7
haaag <81921095+haaag@users.noreply.github.com>
2024-06-14 16:44:59 -0300

    refactor(slice): add `generics`

--- pkg/bookmark/slice.go
+++ pkg/bookmark/slice.go
@@ -1,18 +1,18 @@
 package bookmark
 
-type Slice struct {
-	items *[]Bookmark
+type Slice[T any] struct {
+	items *[]T
 }
 
 // ForEach loops all items
-func (s *Slice) ForEach(fn func(Bookmark)) {
+func (s *Slice[T]) ForEach(fn func(T)) {
 	for _, ele := range *s.items {
 		fn(ele)
 	}
 }
 
 // ForEachErr loops all items and returns a err
-func (s *Slice) ForEachErr(fn func(Bookmark) error) error {
+func (s *Slice[T]) ForEachErr(fn func(T) error) error {
 	for _, ele := range *s.items {
 		if err := fn(ele); err != nil {
 			return err
@@ -22,7 +22,7 @@ func (s *Slice) ForEachErr(fn func(Bookmark) error) error {
 }
 
 // ForEachIdx loop items all items with index
-func (s *Slice) ForEachIdx(fn func(int, Bookmark) error) error {
+func (s *Slice[T]) ForEachIdx(fn func(int, T) error) error {
 	for i, ele := range *s.items {
 		if err := fn(i, ele); err != nil {
 			return err
@@ -31,7 +31,19 @@ func (s *Slice) ForEachIdx(fn func(int, Bookmark) error) error {
 	return nil
 }
 
-func (s *Slice) Has(fn func(Bookmark) bool) bool {
+// Filter
+func (s *Slice[T]) Filter(fn func(T) bool) {
+	var slice = NewSlice[T]()
+	for _, b := range *s.items {
+		t := b
+		if fn(t) {
+			slice.Add(&t)
+		}
+	}
+	*s.items = *slice.items
+}
+
+func (s *Slice[T]) Has(fn func(T) bool) bool {
 	for _, ele := range *s.items {
 		if fn(ele) {
 			return true
@@ -40,7 +52,7 @@ func (s *Slice) Has(fn func(Bookmark) bool) bool {
 	return false
 }
 
-func (s *Slice) Head(n int) {
+func (s *Slice[T]) Head(n int) {
 	l := s.Len()
 	if n <= 0 || n > l || l == 0 {
 		return
@@ -48,7 +60,7 @@ func (s *Slice) Head(n int) {
 	*s.items = (*s.items)[:n]
 }
 
-func (s *Slice) Tail(n int) {
+func (s *Slice[T]) Tail(n int) {
 	l := s.Len()
 	if n <= 0 || n > l || l == 0 {
 		return
@@ -57,40 +69,43 @@ func (s *Slice) Tail(n int) {
 	*s.items = (*s.items)[tail:]
 }
 
-func (s *Slice) Len() int {
+func (s *Slice[T]) Len() int {
 	return len(*s.items)
 }
 
 // Add adds a single item to the items
-func (s *Slice) Add(b *Bookmark) {
+func (s *Slice[T]) Add(b *T) {
 	*s.items = append(*s.items, *b)
 }
 
 // Set sets the items
-func (s *Slice) Set(items *[]Bookmark) {
+func (s *Slice[T]) Set(items *[]T) {
 	*s.items = *items
 }
 
 // Get returns a single item by index
-func (s *Slice) Get(i int) Bookmark {
+func (s *Slice[T]) Get(i int) T {
 	return (*s.items)[i]
 }
 
 // GetAll returns all items in the slice
-func (s *Slice) GetAll() *[]Bookmark {
+func (s *Slice[T]) GetAll() *[]T {
 	return s.items
 }
 
 // IDs returns all items IDs
-func (s *Slice) IDs() []int {
+func (s *Slice[T]) IDs() []int {
 	ids := make([]int, 0, len(*s.items))
-	for _, b := range *s.items {
-		ids = append(ids, b.ID)
+	for _, item := range *s.items {
+		if b, ok := any(item).(Bookmark); ok {
+			ids = append(ids, b.ID)
+		}
 	}
 	return ids
 }
 
 // NewSlice creates a new slice of bookmarks
-func NewSlice() *Slice {
-	return &Slice{items: &[]Bookmark{}}
+func NewSlice[T any]() *Slice[T] {
+	items := make([]T, 0)
+	return &Slice[T]{items: &items}
 }
