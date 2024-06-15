f3afdfe66c1210a47414dbf09d6a7876a29e4ee2
haaag <81921095+haaag@users.noreply.github.com>
2024-06-15 01:18:03 -0300

    refactor(cmd): init dbs

--- cmd/init.go
+++ cmd/init.go
@@ -1,68 +1,75 @@
-// Copyright © 2023 haaag <git.haaag@gmail.com>
 package cmd
 
 import (
-	"errors"
 	"fmt"
-	"log"
-
-	"gomarks/pkg/bookmark"
-	"gomarks/pkg/config"
-	"gomarks/pkg/format"
 
 	"github.com/spf13/cobra"
+
+	"github.com/haaag/gm/pkg/app"
+	"github.com/haaag/gm/pkg/bookmark"
+	"github.com/haaag/gm/pkg/format"
+	"github.com/haaag/gm/pkg/repo"
 )
 
-func printSummary() {
-	config.Version()
-	fmt.Printf("\n+ app home created at: %s\n", format.Text(config.App.Path.Home).Yellow().Bold())
-	fmt.Printf("+ database '%s' initialized\n", format.Text(dbName).Green())
-	fmt.Printf("+ %s bookmark created\n\n", format.Text("initial").Purple())
-}
-
 var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "initialize a new bookmarks database",
+	Use:    "init",
+	Short:  "initialize a new bookmarks database",
+	Hidden: true,
 	RunE: func(_ *cobra.Command, _ []string) error {
-		r, err := bookmark.NewRepository(dbName)
-		if err == nil {
-			return fmt.Errorf("%w: '%s'", bookmark.ErrDBAlreadyInitialized, dbName)
+		r, err := repo.New(Cfg)
+		if r == nil {
+			return fmt.Errorf("init database: %w", err)
+		}
+		defer r.Close()
+		if err := initDB(r); err != nil {
+			return err
 		}
 
-		defer func() {
-			if err := r.DB.Close(); err != nil {
-				log.Printf("closing database: %v", err)
-			}
-		}()
-
-		if !errors.Is(err, bookmark.ErrDBNotFound) {
-			return fmt.Errorf("initializing database: %w", err)
-		}
-
-		if err = config.SetupProjectPaths(); err != nil {
-			return fmt.Errorf("creating home: %w", err)
-		}
-
-		if err = r.Init(); err != nil {
-			return fmt.Errorf("initializing database: %w", err)
-		}
-
-		printSummary()
-
-		bs, err := r.GetAll(config.DB.Table.Main)
+		bookmarks, err := r.GetAll(Cfg.GetTableMain())
 		if err != nil {
 			return fmt.Errorf("getting records: %w", err)
 		}
 
-		if err := handleFormat(bs); err != nil {
-			return fmt.Errorf("%w", err)
-		}
-
-		return nil
+		var bs = bookmark.NewSlice[Bookmark]()
+		bs.Set(bookmarks)
+		Prettify = true
+		return handleFormat(bs)
 	},
 }
 
-//
-// func init() {
-// 	rootCmd.AddCommand(initCmd)
-// }
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+func initDB(r *Repository) error {
+	if r.IsInitialized(r.Cfg.GetTableMain()) && !Force {
+		return fmt.Errorf("%w: '%s'", repo.ErrDBAlreadyInitialized, DBName)
+	}
+	if err := r.Init(); err != nil {
+		return fmt.Errorf("initializing database: %w", err)
+	}
+
+	initialBookmark := bookmark.New(
+		App.Info.URL,
+		App.Info.Title,
+		format.ParseTags(App.Info.Tags),
+		App.Info.Desc,
+	)
+
+	printSummary()
+
+	if _, err := r.Insert(r.Cfg.GetTableMain(), initialBookmark); err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	return nil
+}
+
+func printSummary() {
+	var c = format.Color
+	fmt.Println(app.PrettyVersion(Prettify))
+	fmt.Printf("+ app folder at: %s\n", c(App.Home).Yellow())
+	fmt.Printf("+ %s folder at: %s\n", c("databases").Blue(), format.Color(Cfg.GetHome()).Cyan())
+	fmt.Printf("+ database '%s' initialized\n", c(DBName).Green())
+	fmt.Printf("+ %s bookmark created\n\n", c("initial").Purple())
+}
