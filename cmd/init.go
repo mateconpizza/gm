860a0f48af1eeb1711b7b866cebf53c6e8f8ba7c
haaag <81921095+haaag@users.noreply.github.com>
2025-02-03 18:50:10 -0300

    chore(cmd): minor `init` tweak

--- cmd/init.go
+++ cmd/init.go
@@ -14,7 +14,6 @@ import (
 	"github.com/haaag/gm/internal/handler"
 	"github.com/haaag/gm/internal/menu"
 	"github.com/haaag/gm/internal/repo"
-	"github.com/haaag/gm/internal/slice"
 	"github.com/haaag/gm/internal/sys"
 	"github.com/haaag/gm/internal/sys/files"
 	"github.com/haaag/gm/internal/sys/terminal"
@@ -23,9 +22,8 @@ import (
 var dumpConfig bool
 
 var initCmd = &cobra.Command{
-	Use:    "init",
-	Short:  "initialize a new bookmarks database",
-	Hidden: true,
+	Use:   "init",
+	Short: "initialize a new bookmarks database",
 	RunE: func(_ *cobra.Command, _ []string) error {
 		if dumpConfig {
 			if err := menu.DumpConfig(Force); err != nil {
@@ -34,34 +32,24 @@ var initCmd = &cobra.Command{
 
 			return nil
 		}
-
 		// create paths for the application.
 		p := config.App.Path
-		if err := createPaths(p.Data); err != nil {
+		t := terminal.New()
+		if err := createPaths(t, p.Data); err != nil {
 			return err
 		}
-
 		// init database
 		r, err := repo.New(Cfg)
 		if r == nil {
 			return fmt.Errorf("init database: %w", err)
 		}
 		defer r.Close()
-
-		if err := initDB(r); err != nil {
+		b, err := initDB(r)
+		if err != nil {
 			return err
 		}
-
 		// print new record
-		bs := slice.New[Bookmark]()
-		if err := r.Records(Cfg.Tables.Main, bs); err != nil {
-			return fmt.Errorf("getting records: %w", err)
-		}
-
-		if err := handler.Print(bs); err != nil {
-			return fmt.Errorf("initCmd printer: %w", err)
-		}
-
+		fmt.Print(bookmark.Frame(b))
 		f := frame.New(frame.WithColorBorder(color.Gray))
 		s := color.Gray(Cfg.Name).Italic().String()
 		f.Row().Success("Successfully initialized database " + s).Render()
@@ -76,66 +64,54 @@ func init() {
 }
 
 // createPaths creates the paths for the application.
-func createPaths(path string) error {
+func createPaths(t *terminal.Term, path string) error {
 	f := frame.New(frame.WithColorBorder(color.Gray), frame.WithNoNewLine())
-	f.Header(prettyVersion()).Ln()
-	f.Row().Ln()
-
+	f.Header(prettyVersion()).Ln().Row().Ln()
 	if files.Exists(path) {
 		return nil
 	}
 
 	f.Mid(format.PaddedLine("create path:", fmt.Sprintf("'%s'", path))).Ln()
 	f.Mid(format.PaddedLine("create db:", fmt.Sprintf("'%s'", Cfg.Fullpath()))).Ln()
-	f.Render()
-
+	f.Row().Ln().Render()
 	lines := format.CountLines(f.String())
-
-	q := f.Clean().Row().Ln().Footer("continue?").String()
-	if !terminal.Confirm(q, "y") {
-		return terminal.ErrActionAborted
+	if !t.Confirm(f.Clean().Footer("continue?").String(), "y") {
+		return handler.ErrActionAborted
 	}
-
 	// clean terminal keeping header+row
 	headerN := 3
 	lines += format.CountLines(f.String()) - headerN
-	terminal.ClearLine(lines)
-
+	t.ClearLine(lines)
 	if err := files.MkdirAll(path); err != nil {
 		sys.ErrAndExit(err)
 	}
-
 	f.Clean()
 	f.Success(fmt.Sprintf("Successfully created directory path '%s'.", path)).Ln()
-	f.Success("Successfully created initial bookmark.").Ln()
-	f.Row().Ln()
-	f.Render()
+	f.Success("Successfully created initial bookmark.").Ln().Row().Ln().Render()
 
 	return nil
 }
 
 // initDB creates a new database and populates it with the initial bookmark.
-func initDB(r *repo.SQLiteRepository) error {
+func initDB(r *repo.SQLiteRepository) (*Bookmark, error) {
 	if r.IsInitialized() && !Force {
-		return fmt.Errorf("%w: '%s'", repo.ErrDBAlreadyInitialized, DBName)
+		return nil, fmt.Errorf("%w: '%s'", repo.ErrDBAlreadyInitialized, DBName)
 	}
-
 	if err := r.Init(); err != nil {
-		return fmt.Errorf("initializing database: %w", err)
+		return nil, fmt.Errorf("initializing database: %w", err)
 	}
-
 	// initial bookmark
 	ib := bookmark.New()
 	ib.URL = config.App.Info.URL
 	ib.Title = config.App.Info.Title
 	ib.Tags = bookmark.ParseTags(config.App.Info.Tags)
 	ib.Desc = config.App.Info.Desc
-
+	// insert new bookmark
 	if err := r.Insert(ib); err != nil {
-		return fmt.Errorf("%w", err)
+		return nil, fmt.Errorf("%w", err)
 	}
 
-	return nil
+	return ib, nil
 }
 
 // loadDataPath loads the path to the application's home directory.
